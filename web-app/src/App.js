1e9358f37648f9a81e95eb47e7b80fe41b5dda2a
mintcoo <xhaktm3344@gmail.com>
2023-12-18 23:00:28 +0900

    타입스크립트 설치

--- web-app/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="w-20 h-40 bg-cyan-400">체크</div>;
-}
-
-export default App;
---
+++ web-app/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>체크</div>;
+}
+
+export default App;
