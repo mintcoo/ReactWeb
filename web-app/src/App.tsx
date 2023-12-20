225b77762a83344dfcb49941c46066e04bed8574
mintcoo <xhaktm3344@gmail.com>
2023-12-20 15:24:52 +0900

    유즈네비복습 및 로티에러페이지

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Circle from "./Components/Circle";
 import Layout from "./Layout";
 import Home from "./Screens/Home";
+import NotFound from "./Screens/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/circle" element={<Circle CircleColor={"Tomato"} />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
